41b89b3ea5ad807de6e0d3e2feaf4c8644414869
unknown <mauijaloskor@gmail.com>
2025-02-25 11:31:17 +0600

    dashboard layout design

---
+++ src/app/(withDashboard)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { TChildren } from "@/types/global";
+import React from "react";
+
+const DashboardLayout = ({ children }: TChildren) => {
+  return (
+    <div>
+      DashboardLayout
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
